e0c7581fd759e9bf51a56b6a4e6094ecad6309b2
Im-diablo <adit200515@gmail.com>
2024-07-29 15:02:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,12 @@ from discord.ext import commands
 import gdown
 
 bot = commands.Bot(command_prefix="n", intents=discord.Intents.all())
+url = 'https://drive.google.com/u/0/uc?id=1-rCeT404Do7m8Bu3cqXM6uYaniA661lB'
+  output = 'token.txt'
+  gdown.download(url, output, quiet=False)
 
+  with open('token.txt') as f:
+      TOKEN = f.readline()
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
@@ -24,12 +29,5 @@ async def ping(interaction: discord.Interaction):
 @app_commands.describe(thing_to_say = "synced")
 async def say(interaction: discord.Interaction, thing_to_say: str):
   await interaction.response.send_message(f"synced {interaction.user.mention} said: `{thing_to_say}`")
-  
-  url = 'https://drive.google.com/u/0/uc?id=1-rCeT404Do7m8Bu3cqXM6uYaniA661lB'
-  output = 'token.txt'
-  gdown.download(url, output, quiet=False)
-
-  with open('token.txt') as f:
-      TOKEN = f.readline()
 
 bot.run(TOKEN)
